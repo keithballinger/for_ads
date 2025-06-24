a90b5ab5210e13dd316b07793c2c72f009277127
Keith Ballinger <keithballinger@google.com>
2025-06-24 03:56:10 -0700

    Fix Vite config to resolve index.html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  root: 'frontend',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'frontend/index.html'),
+      },
+    },
+  },
 })
